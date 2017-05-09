85f3efa2f2c91720e8c977ff18789e6f4f84681b
Andrea Barghigiani <andrea@codeat.it>
2017-05-09 10:46:51 +0200

    lezione 10

---
+++ lezione_10/config.rb
@@ -0,0 +1,20 @@
+#Inserisci qua i plugin che vuoi usare
+
+#Sintassi Preferita
+preferred_syntax = :scss
+#Percorso al progetto
+http_path = '/'
+#Cartella CSS
+css_dir = 'css'
+#Cartella SASS
+sass_dir = 'sass'
+#Cartella Immagini
+images_dir = 'img'
+#Cartella JavaScript
+javascripts_dir = 'js'
+#Percorsi Relativi
+relative_assets = true
+#Inserire i Commenti in Linea nel CSS
+line_comments = true
+#Stile di output
+#output_style = :compressed
