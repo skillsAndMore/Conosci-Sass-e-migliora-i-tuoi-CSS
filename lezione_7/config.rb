8999aad8b1ef6e10cdb8d7b3e8a6927a9650abf3
Andrea Barghigiani <andrea@codeat.it>
2017-05-09 10:46:29 +0200

    lezione 7

---
+++ lezione_7/config.rb
@@ -0,0 +1,21 @@
+#Inserisci qua i plugin che vuoi usare
+
+#Sintassi Preferita
+preferred_syntax = :scss
+#Percorso al progetto
+http_path = '/'
+#Cartella CSS
+css_dir = 'css'
+#Cartella SASS
+sass_dir = 'sass'
+#Cartella Immagini
+images_dir = 'images'
+#Cartella JavaScript
+javascripts_dir = 'javascripts'
+#Percorsi Relativi
+relative_assets = false
+#Inserire i Commenti in Linea nel CSS
+line_comments = true
+#Stile di output
+#output_style = :compressed
+sass_options = {:sourcemap => true}
